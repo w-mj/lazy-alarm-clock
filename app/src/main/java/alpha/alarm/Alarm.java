83fc227dedf9f98b76f99fcf81b9bbd6a756414e
WMJ <w.mingjian@yahoo.com>
2017-09-10 21:45:31 +0800

    acceleraometer will not be influenced by gravity

--- app/src/main/java/alpha/alarm/Alarm.java
+++ app/src/main/java/alpha/alarm/Alarm.java
@@ -14,15 +14,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class Alarm extends AppCompatActivity implements SensorEventListener{
     private boolean ringing = false;
+    SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("Alarm", "进入Alarm");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,15 +38,19 @@ public class Alarm extends AppCompatActivity implements SensorEventListener{
             }
         });
 
-        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE); // 获得传感器服务
-        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE); // 获得传感器服务
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
+
+
+        if (MainActivity.DEBUG)
+            ringing = true;
 
         long now = System.currentTimeMillis();
         for (AlarmItemView v : AlarmList.list) {
             if (Math.abs(v.getCalendar().getTimeInMillis() - now) < 5000) {
-                wakePhoneAndUnlock();
+//                wakePhoneAndUnlock();
                 ringing = true;
                 startAlarm();  // 响铃
             }
@@ -51,17 +61,17 @@ public class Alarm extends AppCompatActivity implements SensorEventListener{
             finish();
     }
 
-    //点亮屏幕并解锁
-    private void wakePhoneAndUnlock() {
-        PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
-        PowerManager.WakeLock mWakelock = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.FULL_WAKE_LOCK, "WakeLock");
-        mWakelock.acquire();//唤醒屏幕//......
-        KeyguardManager mManager = (KeyguardManager) this.getSystemService(Context.KEYGUARD_SERVICE);
-        KeyguardManager.KeyguardLock mKeyguardLock = mManager.newKeyguardLock("Lock");
-        mKeyguardLock.disableKeyguard();
-        mWakelock.release();
-        //释放
-    }
+//    //点亮屏幕并解锁
+//    private void wakePhoneAndUnlock() {
+//        PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
+//        PowerManager.WakeLock mWakelock = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.FULL_WAKE_LOCK, "WakeLock");
+//        mWakelock.acquire();//唤醒屏幕//......
+//        KeyguardManager mManager = (KeyguardManager) this.getSystemService(Context.KEYGUARD_SERVICE);
+//        KeyguardManager.KeyguardLock mKeyguardLock = mManager.newKeyguardLock("Lock");
+//        mKeyguardLock.disableKeyguard();
+//        mWakelock.release();
+//        //释放
+//    }
 
     private void startAlarm() {}
     private void stopAlarm() {}
@@ -74,14 +84,14 @@ public class Alarm extends AppCompatActivity implements SensorEventListener{
         if (!ringing)
             return;
 
-        if (sensorType == Sensor.TYPE_ACCELEROMETER) {
+        if (sensorType == Sensor.TYPE_LINEAR_ACCELERATION) {
             float values[] = event.values;
             // 三个方向的加速度值
             float x = values[0];
             float y = values[1];
             float z = values[2];
             int value = 10;
-            // Log.i("out detected", "x=" + x + "y=" + y + "z=" + z);
+            Log.i("out detected", "x=" + x + "y=" + y + "z=" + z);
             if (x >= value || x <= -value || y >= value || y <= -value || z >= value || z <= -value) {
                 Log.i("in detected", "x=" + x + "y=" + y + "z=" + z + "total time=" + String.valueOf(System.currentTimeMillis() - startShakeTime));
                 if (lastDetectedTime == 0 || System.currentTimeMillis() - lastDetectedTime > 2000) {
@@ -103,5 +113,11 @@ public class Alarm extends AppCompatActivity implements SensorEventListener{
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    @Override
+    protected void onPause() {
+        sensorManager.unregisterListener(this);
+        super.onPause();
     }
 }
